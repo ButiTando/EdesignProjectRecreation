0255cee1f4bdc3b78db46fabfead4a7e6410f3f7
butiTando <tando.mahlati2907@gmail.com>
2024-11-29 17:12:25 +0200

    update

--- Solar Efficiency/src/main.cpp
+++ Solar Efficiency/src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_LiquidCrystal.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define I2C_SDA 18
+#define I2C_SCL 19
+
+// Create the LCD object 
+Adafruit_LiquidCrystal lcd(0x20);
+
+// --- Add RTOS tasks parameter declaration --- //
+void lcdTask(void *pvParameters);
+// --- End of RTOS tasks parameter declaration --- //
+
+// --- RTOS handler --- //
+TaskHandle_t lcdTaskHandle;
+// --- End RTOS handler --- //
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin(I2C_SDA, I2C_SCL);
+  lcd.begin(16,2);
+
+  // create lcd task.
+  xTaskCreate(lcdTask, "lcdTask", 1024, NULL, 1, &lcdTaskHandle);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// --- RTOS tasks start --- //
+void lcdTask(void *pvParameters){
+  while(true){
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Hello world");
+
+  }
 }
+// --- End RTOS tasks start --- //
