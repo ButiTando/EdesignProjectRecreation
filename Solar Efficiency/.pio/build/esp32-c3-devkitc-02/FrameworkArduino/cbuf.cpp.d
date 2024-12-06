058194609c7e513981522bed0130f6f225b74d4e
ButiTando <tando.mahlati@gmail.com>
2024-12-06 22:29:30 +0200

    merge fix

--- Solar Efficiency/.pio/build/esp32-c3-devkitc-02/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-c3-devkitc-02/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/23540850/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/23540850/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
